6e3822be9de5d64621e85b19559ee89d83db7b5c
Junjay Tan <junjay.tan@tamr.com>
2015-11-19 17:35:59 -0500

    initial commit

---
+++ src/main/groovy/Gold.groovy
@@ -0,0 +1,28 @@
+/**
+ * Created by junjaytan on 11/19/15.
+ */
+
+// Calculates the golden ratio
+// from Groovy in action 2nd ed: https://github.com/Dierk/GroovyInAction
+
+List fibo = [1, 1]
+List gold = [1, 2]
+
+while (! isGolden( gold[-1]) ) {
+    fibo.add( fibo[-1] + fibo[-2] )
+    gold.add( fibo[-1] / fibo[-2] )
+}
+
+println "found golden ratio with fibo(${ fibo.size-1 }) as"
+println fibo[-1] + " / " + fibo[-2] + " = " + gold[-1]
+println "_" * 10 + "|" + "_" * (10 * gold[-1])
+
+def isGolden(candidate) {
+    def small = 1
+    def big = small * candidate
+    return isCloseEnough( (small+big)/big, big/small)
+}
+
+def isCloseEnough(a,b) {
+    return (a-b).abs() < 1.0e-9
+}
