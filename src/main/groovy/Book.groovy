873788dba42670365a62dd8bccfdcefa56168fe1
Junjay Tan <junjay.tan@tamr.com>
2015-11-23 00:20:54 -0500

    ignore build directory for commits, add Book script

---
+++ src/main/groovy/Book.groovy
@@ -0,0 +1,13 @@
+/**
+ * Created by junjaytan on 11/19/15.
+ */
+
+class Book {
+    private String title
+    Book (String theTitle) {
+        title = theTitle
+    }
+    String getTitle(){
+        return title
+    }
+}
